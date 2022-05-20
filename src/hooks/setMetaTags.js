f0e2a8bbb3c12ce909a3d2e9d7704ffe39a60107
Eomhyunjun <zzp1318@gmail.com>
2022-05-20 22:28:34 +0900

    fixed: 메타태그테스트2

--- src/hooks/setMetaTags.js
+++ src/hooks/setMetaTags.js
@@ -1,8 +1,8 @@
-function setMetaTags(
+function setMetaTags({
   title = '기본 타이틀',
   description = '기본 설명',
-  imageUrl = '기본 사이트 이미지 경로'
-) {
+  imageUrl = '기본 사이트 이미지 경로',
+}) {
   // set title
   document
     .querySelector('meta[property="og:title"]')
